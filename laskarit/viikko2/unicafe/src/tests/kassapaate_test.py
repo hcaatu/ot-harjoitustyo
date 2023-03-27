c37f6b365c9e2d7b543c6bae590987b259613a4b
hcaatu <aatu.hokkanen@helsinki.fi>
2023-03-27 18:17:18 +0300

    kassapääte testi lisätty

---
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -0,0 +1,80 @@
+import unittest
+from kassapaate import Kassapaate
+from maksukortti import Maksukortti
+
+class TestKassapaate(unittest.TestCase):
+    def setUp(self):
+        self.kassapaate = Kassapaate()
+        self.maksukortti = Maksukortti(1000)
+
+    def test_rahamaara_on_oikea(self):
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_lounaita_myyty_nolla(self):
+        self.assertEqual(self.kassapaate.edulliset, 0)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_kassaan_oikea_maara_rahaa_maukkaasti(self):
+        self.kassapaate.syo_maukkaasti_kateisella(500)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100400)
+
+    def test_kassaan_oikea_maara_rahaa_edullisesti(self):
+        self.kassapaate.syo_edullisesti_kateisella(500)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100240)
+    
+    def test_oikea_maara_vaihtorahaa_maukkaasti(self):
+        vaihtoraha = self.kassapaate.syo_maukkaasti_kateisella(500)
+        self.assertEqual(vaihtoraha, 100)
+
+    def test_oikea_maara_vaihtorahaa_edullisesti(self):
+        vaihtoraha = self.kassapaate.syo_edullisesti_kateisella(500)
+        self.assertEqual(vaihtoraha, 260)
+
+    def test_lounaiden_maara_kasvaa_edullisesti(self):
+        self.kassapaate.syo_edullisesti_kateisella(500)
+        self.assertEqual(self.kassapaate.edulliset, 1)
+
+    def test_lounaiden_maara_kasvaa_maukkaasti(self):
+        self.kassapaate.syo_maukkaasti_kateisella(500)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_maksu_ei_riita(self):
+        vaihtoraha = self.kassapaate.syo_maukkaasti_kateisella(200)
+        self.assertEqual(vaihtoraha, 200)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_korttiosto_toimii_edullisesti(self):
+        bool = self.kassapaate.syo_edullisesti_kortilla(self.maksukortti)
+        self.assertEqual(self.maksukortti.saldo, 760)
+        self.assertEqual(bool, True)
+        self.assertEqual(self.kassapaate.edulliset, 1)
+
+    def test_korttiosto_toimii_maukkaasti(self):
+        bool = self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti)
+        self.assertEqual(self.maksukortti.saldo, 600)
+        self.assertEqual(bool, True)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_kortilla_ei_tarpeeksi_saldoa_edullisesti(self):
+        kortti = Maksukortti(200)
+        bool = self.kassapaate.syo_edullisesti_kortilla(kortti)
+        self.assertEqual(self.kassapaate.edulliset, 0)
+        self.assertEqual(kortti.saldo, 200)
+        self.assertEqual(bool, False)
+
+    def test_kortilla_ei_tarpeeksi_saldoa_maukkaasti(self):
+        kortti = Maksukortti(200)
+        bool = self.kassapaate.syo_maukkaasti_kortilla(kortti)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+        self.assertEqual(kortti.saldo, 200)
+        self.assertEqual(bool, False)
+
+    def test_korttiosto_ei_muuta_kassan_maaraa(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.maksukortti)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+        
+    def test_rahan_lautaus_toimii_oikein(self):
+        self.kassapaate.lataa_rahaa_kortille(self.maksukortti, 200)
+        self.assertEqual(self.maksukortti.saldo, 1200)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100200)
