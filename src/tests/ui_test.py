0b4f03362e1e2efc3d7279c2e16788cf2d932c9a
hcaatu <aatu.hokkane@gmail.com>
2023-12-04 23:52:13 +0200

    rm ui_test.py

--- src/tests/ui_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from ui import AppUI
-
-class TestUI(unittest.TestCase):
-    def setUp(self):
-        self.ui = AppUI()
-    
-    def test_screen_correct_resolution(self):
-        self.assertEqual(self.ui.resolution, (1280, 720))
