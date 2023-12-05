726846994ddcdf7244959e2da17b9cb6ae700097
hcaatu <aatu.hokkane@gmail.com>
2023-12-05 15:11:27 +0200

    more tests

--- src/tests/app_test.py
+++ src/tests/app_test.py
@@ -1,4 +1,5 @@
 import unittest
+import tempfile
 from app import App
 from upgrade import CoffeeMaker
 
@@ -6,23 +7,23 @@ class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = App()
         self.app.score = 100
+        self.file_path = tempfile.NamedTemporaryFile(delete=False).name
 
     def test_constructor_functions_sets_correct_values(self):
         self.assertEqual(self.app.score, 100)
         self.assertEqual(self.app.tickrate, 60)
         self.assertEqual(self.app.profit, None)
+        self.assertEqual(self.app.time_played, 0)
 
     def test_buy_upgrade(self):
         upgrade = CoffeeMaker()
 
-        # save initial values
         initial_upgrades = {"coffee_maker" : 0}
         initial_score = self.app.score
         initial_cost = upgrade.cost
 
         self.app.buy_upgrade(upgrade, upgrade.cost)
 
-        # check if the values are as expected
         self.assertEqual(self.app.score, initial_score - initial_cost)
         self.assertEqual(self.app.upgrades[upgrade.name], initial_upgrades[upgrade.name] + 1)
         self.assertEqual(self.app.cost[upgrade.name], initial_cost * 1.2)
@@ -40,3 +41,9 @@ class TestApp(unittest.TestCase):
         self.app.profit = 100
         self.app.apply_profit()
         self.assertEqual(self.app.score, initial_score + self.app.profit/self.app.tickrate)
+
+    def test_profit_doesnt_apply_if_there_is_no_profit(self):
+        initial_score = self.app.score
+        self.app.profit = 0
+        self.app.apply_profit()
+        self.assertEqual(self.app.score, initial_score)
--- src/tests/repository_test.py
+++ src/tests/repository_test.py
@@ -1,9 +1,46 @@
 import unittest
-from app import App
-from repository import Repository
+import os
+import tempfile
+from repository import SaveFile, Repository
 
-class RepositoryTest(unittest.TestCase):
+class TestRepository(unittest.TestCase):
+    # intialize with temporary files, so that tests work without dependencies
     def setUp(self):
-        self.app = App()
+        self.file = tempfile.NamedTemporaryFile(delete=False)
+        self.file_path = self.file.name
+        self.repository = Repository(self.file_path)
+        self.repository.delete_all()
+
+    def test_save_and_load(self):
+        save_file = SaveFile(200, {'coffee_maker': 10, 'brewer': 5}, {'coffee_maker': 20, 'brewer': 50}, 500)
+
+        saved_data = self.repository.save(save_file)
+
+        self.assertEqual(saved_data.score, save_file.score)
+        self.assertEqual(saved_data.upgrades, save_file.upgrades)
+        self.assertEqual(saved_data.cost, save_file.cost)
+        self.assertEqual(saved_data.time_played, save_file.time_played)
+
+        loaded_data = self.repository.load()
+
+        self.assertEqual(loaded_data.score, save_file.score)
+        self.assertEqual(loaded_data.upgrades, save_file.upgrades)
+        self.assertEqual(loaded_data.cost, save_file.cost)
+        self.assertEqual(loaded_data.time_played, save_file.time_played)
+
+    def test_ensure_file_exists(self):
+        self.repository._ensure_file_exists()
+        self.assertEqual(os.path.exists(self.file_path), True)
+        
+    def test_read_and_write(self):
+        save_file = SaveFile(200, {'coffee_maker': 10, 'brewer': 5}, {'coffee_maker': 20, 'brewer': 50}, 500)
+        
+        self.repository._write(save_file)
+        loaded_data = self.repository._read()
+
+        self.assertEqual(loaded_data.score, save_file.score)
+        self.assertEqual(loaded_data.upgrades, save_file.upgrades)
+        self.assertEqual(loaded_data.cost, save_file.cost)
+        self.assertEqual(loaded_data.time_played, save_file.time_played)
         
     
